a2e88ef0b7f6b81e328e475d29d0f0b89a2df4d6
sihle <sihlesithole810@gmail.com>
2024-07-09 21:21:52 +0200

    siyethaba datasource

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,12 @@
+
+const navbarToggleBtn = document.getElementById('navbarToggleBtn');
+const navbarLinks = document.querySelector('.navbar');
+
+navbarToggleBtn.addEventListener('click', function() {
+    navbarLinks.classList.toggle('active');
+});
+
+
 var typed = new Typed(".multiple-text" ,{
 	strings: ["Frontend Developer" , "Backend Developer" , "Database administrator" , "Database development"],
 	typeSpeed:100,
@@ -5,3 +14,6 @@ var typed = new Typed(".multiple-text" ,{
 	backDelay:1000,
 	loop: true
 })
+
+
+
